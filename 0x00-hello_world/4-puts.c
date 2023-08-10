ccac2096570e0e6379a30cda52719912c011a330
Jadline <Jadline>
2023-08-10 06:39:06 -0700

    the hello world message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
-a program that prints out a message to the screen  
-* */
-int main()
+*a program that prints out a message using the puts function
+*return 0 (success)
+*/
+int main(void)
 {
- puts('"Programming is like building a multilingual puzzle');
- return 0;
+puts('"Programming is like building a multilingual puzzle');
+return (0);
 }
