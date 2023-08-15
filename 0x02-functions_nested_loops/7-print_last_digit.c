eef7a65df0fc1b7b8aa10b6d9e4f05cf904033fe
Jadline <Jadline>
2023-08-15 06:39:15 -0700

    prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - A program that prints the last digit
+ * Description: Prints the last digit of a number
+ * @k: Variable name
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int k)
+{
+	int last_digit  = (k % 10);
+
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;
+	}
+	return (last_digit);
+}
