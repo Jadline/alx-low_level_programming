7c754e1e2109aca2e2536d9fa1db2c5b737ed8f5
Jadline <Jadline>
2023-08-15 08:39:09 -0700

    adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - A program that adds two integers
+ *
+ * @i: Variable name
+ * @j: Variable name
+ *
+ * Return: Will return the sume of the two integers
+ */
+int add(int i, int j)
+{
+	int result = i + j;
+
+	return (result);
+}
