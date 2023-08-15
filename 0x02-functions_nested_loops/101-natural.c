065e095dbc863746eea012542cfadc7138dfebac
Jadline <Jadline>
2023-08-15 10:34:42 -0700

    fizzbuzz

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Computes sum of multiples of 3 and 5
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int p, z;
+
+	for (z = 0; z < 1024; z++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			p == z;
+	}
+	printf("%d\n", p);
+	return (0);
+}
