04ccce67e5d3f0a5f9a546fbbdf519f569bd85e8
Jadline <Jadline>
2023-08-15 10:18:19 -0700

    printing table of n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_times_table - A program to print table of n
+ *
+ * @n: starting number
+ */
+void print_times_table(int n)
+{
+	int z, p, k;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (k = 0; k <= n; ++k)
+		{
+			_putchar(',');
+			for (p = 1; p <= n; ++p)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				z = k * p;
+				if (z <= 9)
+					_putchar(' ');
+				if (z <= 99)
+					_putchar(' ');
+				if (z >= 100)
+				{
+					_putchar((z / 100) + 48);
+					_putchar((z / 10) % 10 + 48);
+				}
+				else if (z <= 99 && z >= 10)
+					_putchar((z / 10) + 48);
+				_putchar((z % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
+
+
