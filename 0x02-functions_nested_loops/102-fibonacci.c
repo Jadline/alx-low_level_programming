8ca7590f593723ee7c16d0c070584daf1a67773a
Jadline <Jadline>
2023-08-15 11:02:16 -0700

    fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the sum of fibonaccis
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long fibon1 = 0, fibon2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = fibon1 + fibon2;
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			total_sum += sum;
+
+		fibon1 = fibon2;
+		fibon2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
