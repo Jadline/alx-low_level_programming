4f56d97460b7737798cb33dfd6d84edbfebbac6d
Jadline <Jadline>
2023-08-14 23:43:14 -0700

    printing out putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,3 +9,4 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
