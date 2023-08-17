abba6c49c9208b87dfe72df5af421253fe866b0f
Jadline <Jadline>
2023-08-17 02:35:49 -0700

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  mul - A program to multiply two integers
+ *  @a: variable name
+ *  @b: variable name
+ *  Return: the result
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
