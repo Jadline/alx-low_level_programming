a91af3a94504a555a5f4050101a7d831cb1cdf96
Jadline <Jadline>
2023-08-17 07:42:21 -0700

    prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,25 +4,19 @@
  */
 void more_numbers(void)
 {
-	int i = 0;
+	int i;
 	int j;
 
-	while (i < 10)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 10)
+			if (j > 9)
 			{
-				_putchar('1');
-				_putchar(j % 10 + '0');
+				_putchar(j / 10 + '0');
 			}
-			else
-			{
-				_putchar(j + '0');
-			}
-		}
+			_putchar(j % 10 + '0');
+		}	
 		_putchar('\n');
-		i++;
 	}
 }
-
