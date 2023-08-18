cb173f230cc538d5c37984936cee37444da1f40e
Jadline <Jadline>
2023-08-17 19:39:12 -0700

    leap years

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,26 +12,26 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	int daysInMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	int dayOfYear = day;
-	int i;
+    if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
+    {
+        if (month > 2 && day >= 60)
+        {
+            day++;
+        }
 
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-	{
-		daysInMonth[2] = 29;
-		}
-
-	if (day > daysInMonth[month])
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		return;
-	}
-	
-	for (i = 1; i < month; i++)
-	{
-		dayOfYear += daysInMonth[i];
-	}
-	printf("Day of the year: %d\n", dayOfYear);
-	printf("Remaining days: %d\n", daysInMonth[2] - day + 1);
+        printf("Day of the year: %d\n", day);
+        printf("Remaining days: %d\n", 366 - day);
+    }
+    else
+    {
+        if (month == 2 && day == 60)
+        {
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+        }
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 365 - day);
+        }
+    }
 }
-
