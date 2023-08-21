a4a5880fb4e1b4e1a57ff26287c71f8bf5b478f2
Jadline <Jadline>
2023-08-21 00:26:13 -0700

    prints half of a strin

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to a string
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int start;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	start = (length + 1) / 2;
+
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+	_putchar('\n');
+}
